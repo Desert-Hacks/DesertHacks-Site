debd0ef4b4a6fdac31ddb28768ceb02da24304d7
Wen <wenterr@amazon.com>
2017-07-20 16:43:02 -0700

    last yellow i swear

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let colors = ["#B52945", "#EAC26B", "#5B9BA5", "#574D68"];
-  let hovers = ["#DB8A9A", "#F2DEB3", "#A7CBD1", "#C3BCD1"];
+  let colors = ["#B52945", "#F9D178", "#5B9BA5", "#574D68"];
+  let hovers = ["#DB8A9A", "#FFE4AA", "#A7CBD1", "#C3BCD1"];
   let randInt = Math.floor((Math.random() * 4));
 
   document.body.style.backgroundColor = colors[randInt];
