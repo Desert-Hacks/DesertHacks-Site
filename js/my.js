dbfdcf6a98895fa41c0827d8a449e9bc40a67068
Wen <wenterr@amazon.com>
2017-07-20 16:25:59 -0700

    omg this hover color thing

--- js/my.js
+++ js/my.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let colors = ["#B52945", "#EFC873", "#64AA65", "#5B9BA5", "#574D68"];
+  let colors = ["#B52945", "#DDB96A", "#5B9BA5", "#574D68"];
+  let hovers = ["#DB8A9A", "#F2DEB3", "#A7CBD1", "#C3BCD1"];
+  let randInt = Math.floor((Math.random() * 4));
 
-  document.body.style.backgroundColor = colors[Math.floor((Math.random() * 5))];
+  document.body.style.backgroundColor = colors[randInt];
+
+  let style = document.createElement("style");
+  // WebKit hack
+  style.appendChild(document.createTextNode(""));
+  // Add the <style> element to the page
+  document.head.appendChild(style);
+  style.sheet.insertRule(`a i:hover{ color: ${hovers[randInt]}`, 0);
 });
