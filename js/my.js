1d227f0d0fb633d04fabef11c743d890fe7d1581
Wen <wenterr@amazon.com>
2017-07-20 15:41:13 -0700

    fixed colors

--- js/my.js
+++ js/my.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let colors = ["#B52945", "#F9D178", "#64AA65", "#5B9BA5"];
-  let randInt = Math.floor((Math.random() * 4) + 1);
+  let colors = ["#B52945", "#EFC873", "#64AA65", "#5B9BA5", "#574D68"];
 
-  document.body.style.backgroundColor = colors[randInt];
+  document.body.style.backgroundColor = colors[Math.floor((Math.random() * 5))];
 });
